6fa27f353fb953aca19b04710a9ea6fec7bf0f36
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-03-14 20:59:31 -0400

    made changes to word to guess function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // number of hangman options 
-var nflTeamNames = ["cardinals", "falcons", "ravens", "bills", "panthers", "bears", "bengals", "browns", 
-"cowboys", "broncos", "lions", "packers", "texans", "colts", "jaguars", "chiefs", "chargers", "rams",
-"dolphins", "vikings", "patriots", "saints", "giants", "jets", "raiders", "eagles", "steelers", "49ers", 
-"buccaneers", "titans", "redskins"];
+var nflTeamNames = ["49ers", "bears", "bengals", "bills", "broncos", "browns", "buccaneers",
+"cardinals", "chargers", "chiefs", "colts", "cowboys", "dolphins", "eagles", "falcons", 
+"giants", "jaguars", "jets", "lions", "packers", "panthers", "patriots", "raiders", "rams",
+"ravens", "redskins", "saints", "seahawks", "steelers", "texans", "titans", "vikings"]
 var correctLetters = [];
 var alreadyUsedLetters = [];
 var wrongGuesses = [];
@@ -10,37 +10,77 @@ var numberOfGuesses = 8;
 var letterLocation;
 //random team selection
 var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
+var numberOfLetters = 0;
 
 // for finding multiple letters in a given word
-function pushAllIndexes(arr, val) {
+function letterIndexAssign(arr, val) {
     // var indexes = [], i;
     for(i = 0; i < arr.length; i++){
         if (arr[i] === val){
             correctLetters[i] = val;
-            // return indexes;
+            numberOfLetters++; //numberOfLetters is written here because of mulitple
+            // instances of one letter
         }
+        // else {
+        //     correctLetters[i] = " ";
+        // }
     }
 }
 
-console.log(randomTeamName);
+// function displayCorrectLetters(){
+//     for(var i = 0; i < correctLetters.length; i++){
+//         var currentLetters = currentLetters + correctLetters[i];
+//     }
+//     document.getElementById("correct-letters").innerHTML = currentLetters;
+// }
+
+// function displayIncorrectLetters(){
+//     for (var i = 0; i < wrongGuesses.length; i++){
+//         var wrongLetters = wrongLetters + wrongGuesses[i] + " ";     
+//     }
+//     document.getElementById("wrong-guesses").innerHTML = wrongLetters;
+// }
+
+console.log(randomTeamName); //delete when program is finished
+    // create dash lines for number of letters
+
+document.getElementById("correct-letters").innerHTML = 
+
+randomTeamName.foreach(){
+    
+}
+
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
     //Remember to remember numbers i.e. 49ers
     // makes sure key is in choices and hasn't been chosen already
+    // letterIndexAssign(randomTeamName, userGuess);
     if(randomTeamName.includes(userGuess) && !correctLetters.includes(userGuess)){
         // insert userGuess.indexof method here
-        pushAllIndexes(randomTeamName, userGuess)
+        letterIndexAssign(randomTeamName, userGuess);
         // letterLocation = randomTeamName.indexOf(userGuess);
         // correctLetters[letterLocation] = userGuess;
         alreadyUsedLetters.push(userGuess);
-        document.querySelector("#team-name").innerHTML = "<strong>Word to Guess: <strong>" + correctLetters;
+        // document.querySelector("#team-name").innerHTML = "<strong>Word to Guess: <strong>" + correctLetters;
+        // displayCorrectLetters();
+        document.querySelector("#correct-letters").innerHTML = correctLetters.join("");
+        if(numberOfLetters === randomTeamName.length){
+            document.querySelector("#result").innerHTML = "Congratulations, you won!";
+        }
     }
     else if(!randomTeamName.includes(userGuess) && !alreadyUsedLetters.includes(userGuess)){
-        alreadyUsedletters.push(userGuess);
+        alreadyUsedLetters.push(userGuess);
         wrongGuesses.push(userGuess);
-        document.querySelector("#wrong-guesses").innerHTML = "Wrong Guesses: " + wrongGuesses;
+        document.querySelector("#wrong-guesses").innerHTML = wrongGuesses.join(" ");
         numberOfGuesses--;
-        document.querySelector("#remaining-guesses").innerHTML = "Remaining Guesses: " + numberOfGuesses;
+        document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
+        if(numberOfGuesses === 0){
+            document.querySelector("#result").innerHTML = "Sorry, you lost. Better luck next time!";
+        }
     }
 }
+
+//end of game conditionals
+
+
         
