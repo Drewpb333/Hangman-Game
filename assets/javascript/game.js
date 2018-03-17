5d7dc1bc47c916e78f00668dbcde7b478b288f7a
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-03-17 14:54:18 -0400

    3.17.2018 update to css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,49 @@
-// number of hangman options 
-var nflTeamNames = ["49ers", "bears", "bengals", "bills", "broncos", "browns", "buccaneers",
-"cardinals", "chargers", "chiefs", "colts", "cowboys", "dolphins", "eagles", "falcons", 
-"giants", "jaguars", "jets", "lions", "packers", "panthers", "patriots", "raiders", "rams",
-"ravens", "redskins", "saints", "seahawks", "steelers", "texans", "titans", "vikings"]
-var correctLetters = [];
+var nflTeamNames=["49ers", "bears", "bengals", "bills", "broncos", "browns", "buccaneers",
+    "cardinals", "chargers", "chiefs", "colts", "cowboys", "dolphins", "eagles", "falcons", 
+    "giants", "jaguars", "jets", "lions", "packers", "panthers", "patriots", "raiders", "rams",
+    "ravens", "redskins", "saints", "seahawks", "steelers", "texans", "titans", "vikings"];
+var correctLetters= [];
 var alreadyUsedLetters = [];
 var wrongGuesses = [];
 var numberOfGuesses = 8;
-var letterLocation;
-//random team selection
-var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
 var numberOfLetters = 0;
+// random team selection
+var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
 
-// for finding multiple letters in a given word
 function letterIndexAssign(arr, val) {
-    // var indexes = [], i;
     for(i = 0; i < arr.length; i++){
         if (arr[i] === val){
             correctLetters[i] = val;
-            numberOfLetters++; //numberOfLetters is written here because of mulitple
-            // instances of one letter
+            numberOfLetters++; 
         }
-        // else {
-        //     correctLetters[i] = " ";
-        // }
-    }
+    };
 }
 
-// function displayCorrectLetters(){
-//     for(var i = 0; i < correctLetters.length; i++){
-//         var currentLetters = currentLetters + correctLetters[i];
-//     }
-//     document.getElementById("correct-letters").innerHTML = currentLetters;
-// }
+for(var i=0; i < randomTeamName.length; i++){
+    correctLetters[i] = "_ ";
+};
 
-// function displayIncorrectLetters(){
-//     for (var i = 0; i < wrongGuesses.length; i++){
-//         var wrongLetters = wrongLetters + wrongGuesses[i] + " ";     
-//     }
-//     document.getElementById("wrong-guesses").innerHTML = wrongLetters;
-// }
+function resetGame(){
+    var correctLetters= [];
+    var alreadyUsedLetters = [];
+    var wrongGuesses = [];
+    
+    var numberOfGuesses = 8;
+    document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
+    var numberOfLetters = 0;
+    // random team selection
+    var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
+    console.log(randomTeamName);
+}
 
 console.log(randomTeamName); //delete when program is finished
-    // create dash lines for number of letters
-
-document.getElementById("correct-letters").innerHTML = 
-
-randomTeamName.foreach(){
-    
-}
 
 document.onkeyup = function(event) {
     var userGuess = event.key.toLowerCase();
     //Remember to remember numbers i.e. 49ers
     // makes sure key is in choices and hasn't been chosen already
     // letterIndexAssign(randomTeamName, userGuess);
+    document.querySelector("#correct-letters").innerHtml = correctLetters.join("");
     if(randomTeamName.includes(userGuess) && !correctLetters.includes(userGuess)){
         // insert userGuess.indexof method here
         letterIndexAssign(randomTeamName, userGuess);
@@ -65,7 +54,10 @@ document.onkeyup = function(event) {
         // displayCorrectLetters();
         document.querySelector("#correct-letters").innerHTML = correctLetters.join("");
         if(numberOfLetters === randomTeamName.length){
-            document.querySelector("#result").innerHTML = "Congratulations, you won!";
+            $("#result").innerHTML = "Congratulations, you won!";
+             $("#hangman-image").html("<div class='thumbnail'><img src='assets/images/" + randomTeamName + ".jpg' \
+             alt='Team Logo'></div>");
+            resetGame();
         }
     }
     else if(!randomTeamName.includes(userGuess) && !alreadyUsedLetters.includes(userGuess)){
@@ -76,6 +68,7 @@ document.onkeyup = function(event) {
         document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
         if(numberOfGuesses === 0){
             document.querySelector("#result").innerHTML = "Sorry, you lost. Better luck next time!";
+            resetGame();
         }
     }
 }
@@ -83,4 +76,4 @@ document.onkeyup = function(event) {
 //end of game conditionals
 
 
-        
+    
