ec3060ce7469c23c04083b392148bf766cdcf55c
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-03-18 20:20:58 -0400

    css changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var numberOfGuesses = 8;
 var numberOfLetters = 0;
 // random team selection
 var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
+var initializeGame = true;
 
 function letterIndexAssign(arr, val) {
     for(i = 0; i < arr.length; i++){
@@ -16,64 +17,73 @@ function letterIndexAssign(arr, val) {
             correctLetters[i] = val;
             numberOfLetters++; 
         }
-    };
-}
+    }
+};
 
-for(var i=0; i < randomTeamName.length; i++){
+function blankSpaces(){
+    for(var i=0; i < randomTeamName.length; i++){
     correctLetters[i] = "_ ";
+    };
+    $("correct-letters").text = correctLetters;
 };
 
 function resetGame(){
-    var correctLetters= [];
-    var alreadyUsedLetters = [];
-    var wrongGuesses = [];
-    
-    var numberOfGuesses = 8;
+    setTimeout(function(){
+        initializeGame = false;
+    }, 4000);
+    correctLetters= [];
+    alreadyUsedLetters = [];
+    wrongGuesses = [];
+    $("#wrong-guesses").empty();
+    numberOfGuesses = 8;
     document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
-    var numberOfLetters = 0;
+    numberOfLetters = 0;
     // random team selection
-    var randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
+    randomTeamName = nflTeamNames[Math.floor(Math.random() * nflTeamNames.length)];
+    blankSpaces();
+    initializeGame = true;
     console.log(randomTeamName);
 }
 
-console.log(randomTeamName); //delete when program is finished
 
-document.onkeyup = function(event) {
-    var userGuess = event.key.toLowerCase();
-    //Remember to remember numbers i.e. 49ers
-    // makes sure key is in choices and hasn't been chosen already
-    // letterIndexAssign(randomTeamName, userGuess);
-    document.querySelector("#correct-letters").innerHtml = correctLetters.join("");
-    if(randomTeamName.includes(userGuess) && !correctLetters.includes(userGuess)){
-        // insert userGuess.indexof method here
-        letterIndexAssign(randomTeamName, userGuess);
-        // letterLocation = randomTeamName.indexOf(userGuess);
-        // correctLetters[letterLocation] = userGuess;
-        alreadyUsedLetters.push(userGuess);
-        // document.querySelector("#team-name").innerHTML = "<strong>Word to Guess: <strong>" + correctLetters;
-        // displayCorrectLetters();
-        document.querySelector("#correct-letters").innerHTML = correctLetters.join("");
-        if(numberOfLetters === randomTeamName.length){
-            $("#result").innerHTML = "Congratulations, you won!";
-             $("#hangman-image").html("<div class='thumbnail'><img src='assets/images/" + randomTeamName + ".jpg' \
-             alt='Team Logo'></div>");
-            resetGame();
+console.log(randomTeamName); //delete when program is finished
+blankSpaces();
+console.log(correctLetters);
+
+if(initializeGame){
+    document.onkeyup = function(event) {
+        var userGuess = event.key.toLowerCase();
+        // letterIndexAssign(randomTeamName, userGuess);
+        document.querySelector("#correct-letters").innerHtml = correctLetters.join("");
+        if(randomTeamName.includes(userGuess) && !correctLetters.includes(userGuess)){
+            // insert userGuess.indexof method here
+            letterIndexAssign(randomTeamName, userGuess);
+            // letterLocation = randomTeamName.indexOf(userGuess);
+            alreadyUsedLetters.push(userGuess);
+            document.querySelector("#correct-letters").innerHTML = correctLetters.join("");
+            if(numberOfLetters === randomTeamName.length){
+                $("#result").html("Congratulations, you won!");
+                $("#hangman-image").html("<div class='thumbnail'><img src='assets/images/" + randomTeamName + ".jpg' \
+                alt='Team Logo'></div>" + correctLetters.join(""));
+                resetGame();
+                initializeGame = false;
+            }
         }
-    }
-    else if(!randomTeamName.includes(userGuess) && !alreadyUsedLetters.includes(userGuess)){
-        alreadyUsedLetters.push(userGuess);
-        wrongGuesses.push(userGuess);
-        document.querySelector("#wrong-guesses").innerHTML = wrongGuesses.join(" ");
-        numberOfGuesses--;
-        document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
-        if(numberOfGuesses === 0){
-            document.querySelector("#result").innerHTML = "Sorry, you lost. Better luck next time!";
-            resetGame();
+        else if(!randomTeamName.includes(userGuess) && !alreadyUsedLetters.includes(userGuess)){
+            alreadyUsedLetters.push(userGuess);
+            wrongGuesses.push(userGuess);
+            document.querySelector("#wrong-guesses").innerHTML = wrongGuesses.join(" ");
+            numberOfGuesses--;
+            document.querySelector("#remaining-guesses").innerHTML = numberOfGuesses;
+            if(numberOfGuesses <= 0){
+                document.querySelector("#result").innerHTML = "Sorry, you lost. Better luck next time!";
+                resetGame();
+                initializeGame = false;
+            }
         }
     }
 }
 
-//end of game conditionals
 
 
     
